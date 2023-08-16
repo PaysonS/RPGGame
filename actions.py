283dd12fb6f1e478be97606834da30e3422b6b12
PaysonS <pstory1001@gmail.com>
2023-08-16 14:58:49 -0700

    pushed actions.py

--- actions.py
+++ actions.py
@@ -1,9 +1,20 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from engine import Engine
+    from entity import Entity
+
+
 class Action:
-    pass
+    def perform(self, engine: Engine, entity: Entity)->None:
+        raise NotImplementedError()
 
 
 class EscapeAction(Action):
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise SystemExit()
 
 
 
@@ -14,4 +25,12 @@ class MovementAction(Action):
         self.dx = dx
         self.dy = dy
         
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
         
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            return 
+        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            return
+        entity.move(self.dx, self.dy) 
