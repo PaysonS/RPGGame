8c8e9d035c4816d7028e1e38fd71a1c55cbd7bfa
PaysonS <pstory1001@gmail.com>
2023-08-16 22:04:45 -0700

    added visibility

--- game_map.py
+++ game_map.py
@@ -8,10 +8,16 @@ class GameMap:
     def __init__(self, width: int, height: int):
         self.width, self.height = width, height
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
-
+        self.visible = np.full((width,height),fill_value=False, order="F") # tiles you can see 
+        self.explored = np.full((width,height),fill_value=False, order="F")# tiles youve been to
+        
 
     def in_bounds(self, x: int, y: int) -> bool:
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None:
-        console.tiles_rgb[0 : self.width, 0 : self.height] = self.tiles["dark"]
+        console.tiles_rgb[0 : self.width, 0 : self.height] = np.select(
+            condlist=[self.visible,self.explored],
+            choicelist=[self.tiles["light"],self.tiles["dark"]],
+            default=tile_types.SHROUD
+        )
