98bb7582d6ef6bad3a06d898260739fe6051f9ae
PaysonS <pstory1001@gmail.com>
2023-08-16 22:04:57 -0700

    added visibility

--- tile_types.py
+++ tile_types.py
@@ -1,6 +1,6 @@
 from typing import Tuple
 
-import numpy as np #type: ignore
+import numpy as np  # type: ignore
 
 graphic_dt = np.dtype(
     [
@@ -16,6 +16,7 @@ tile_dt = np.dtype(
         ("walkable", np.bool_),
         ("transparent", np.bool_),
         ("dark", graphic_dt),
+        ("light", graphic_dt),
     ]
 )
 
@@ -25,17 +26,25 @@ def new_tile(
     walkable: int,
     transparent: int,
     dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+    light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
-    return np.array((walkable, transparent, dark), dtype=tile_dt)
+    return np.array((walkable, transparent, dark, light), dtype=tile_dt)
+
+
+SHROUD = np.array(
+    (ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt
+)  # unseen and unexplored tiles
 
 
 floor = new_tile(
     walkable=True,
     transparent=True,
     dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
+    light=(ord(" "), (255, 255, 255), (200, 180, 50)),
 )
 wall = new_tile(
     walkable=False,
     transparent=False,
     dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
+    light=(ord(" "), (255, 255, 255), (130, 110, 50)),
 )
